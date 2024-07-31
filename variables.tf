32af41d1c2777dff4fc3d1b65e6cd5260d4e8374
saransh0007 <40159583+saransh0007@users.noreply.github.com>
2024-07-31 21:10:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-resource "google_network_connectivity_service_connection_policy" "default" {
-  name          = var.name
-  location      = var.location
-  service_class = var.service_class
-  network       = var.network
-
-  dynamic "psc_config" {
-    for_each = var.psc_config
-    content {
-      subnetworks = psc_config.value.subnetworks
-      limit       = psc_config.value.limit
-    }
-  }
-}
-
 # modules/service_connection_policy/variables.tf
 variable "name" {
   type        = string
