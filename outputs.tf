0a6c181954728cd333fc9ebdb2e9d209316e30d4
saransh0007 <40159583+saransh0007@users.noreply.github.com>
2024-08-02 00:19:50 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,3 @@ output "id" {
   value       = google_network_connectivity_service_connection_policy.default.id
   description = "ID of the created service connection policy"
 }
-
-output "self_link" {
-  value       = google_network_connectivity_service_connection_policy.default.self_link
-  description = "Self-link of the created service connection policy"
-}
